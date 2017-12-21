b895173a5ce6aeec4a1d74c112ea2bea41a24ea4
Adam Campbell <adamc@palantir.com>
2017-12-20 23:41:00 -0500

    Add generated single page iOS Swift app from Xcode

---
+++ iOS/LeafByte/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeafByte
+//
+//  Created by Adam Campbell on 12/20/17.
+//  Copyright © 2017 Blue Folder Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
