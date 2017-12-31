3ba268221f04c977deb76bf4a0c10fd5bba22781
Adam Campbell <adamc@palantir.com>
2017-12-30 23:11:31 -0500

    Allow drawing on image

--- iOS/LeafByte/ViewControllers/LBFillHolesViewController.swift
+++ iOS/LeafByte/ViewControllers/LBFillHolesViewController.swift
@@ -6,10 +6,10 @@
 //  Copyright © 2017 The Blue Folder Project. All rights reserved.
 //
 
-
+import CoreGraphics
 import UIKit
 
-class LBFillHolesViewController: UIViewController, UINavigationControllerDelegate, UIScrollViewDelegate {
+class LBFillHolesViewController: UIViewController {
     
     var baseImage: UIImage?
     
@@ -21,4 +21,49 @@ class LBFillHolesViewController: UIViewController, UINavigationControllerDelegat
     }
     
     @IBOutlet weak var baseImageView: UIImageView!
+    @IBOutlet weak var drawingImageView: UIImageView!
+    
+    var swiped = false
+    var lastPoint = CGPoint.zero
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = false
+        lastPoint = (touches.first?.location(in: drawingImageView))!
+    }
+    
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+        UIGraphicsBeginImageContext(drawingImageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(cyan: 100,
+                              magenta: 100,
+                              yellow: 100,
+                              black: 100,
+                              alpha: 100)
+        
+        drawingImageView.image?.draw(in: CGRect(x: 0, y: 0, width: drawingImageView.frame.size.width, height: drawingImageView.frame.size.height))
+        
+        context!.move(to: fromPoint)
+        context!.addLine(to: toPoint)
+        context!.strokePath()
+        
+        drawingImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = true
+        let currentPoint = touches.first?.location(in: drawingImageView)
+        drawLineFrom(fromPoint: lastPoint, toPoint: currentPoint!)
+        
+        lastPoint = currentPoint!
+        
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !swiped {
+            drawLineFrom(fromPoint: lastPoint, toPoint: lastPoint)
+        }
+    }
 }
