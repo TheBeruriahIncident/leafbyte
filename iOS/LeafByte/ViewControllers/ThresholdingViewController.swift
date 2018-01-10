66805df620bbaefee951bcdefe830c7a45f692ae
Adam Campbell <adamc@palantir.com>
2018-01-09 23:37:22 -0500

    Use connected components function in threshold view

--- iOS/LeafByte/ViewControllers/ThresholdingViewController.swift
+++ iOS/LeafByte/ViewControllers/ThresholdingViewController.swift
@@ -98,105 +98,12 @@ class ThresholdingViewController: UIViewController, UIScrollViewDelegate {
     
     func findScale() {
         let image = IndexableImage(uiToCgImage(baseImageView.image!))
-        let width = image.width
-        let height = image.height
+        let booleanImage = BooleanIndexableImage(width: image.width, height: image.height)
+        booleanImage.addImage(image, withPixelToBoolConversion: { $0.isNonWhite() })
         
-        var groupToPoint = [Int: (Int, Int)]()
-        var groupIds = Array(repeating: Array(repeating: 0, count: width), count: height)
-        var occupiedGroup = 1
-        var emptyGroup = -1
+        let connectedComponentsInfo = labelConnectedComponents(image: booleanImage)
         
-        var groupSizes = [Int: Int]()
-        
-        // TODO: how to best represent this??
-        var equivalentGroups = [Set<Int>]()
-        
-        for y in 0...height - 1 {
-            for x in 0...width - 1 {
-                let occupied = image.getPixel(x: x, y: y).isNonWhite()
-                
-                // TODO: consider 8 connectivity instead of 4
-                // using 4-connectvity
-                let westGroup = x > 0 && occupied == image.getPixel(x: x - 1, y: y).isNonWhite()
-                    ? groupIds[y][x - 1]
-                    : nil
-                let northGroup = y > 0 && occupied == image.getPixel(x: x, y: y - 1).isNonWhite()
-                    ? groupIds[y - 1][x]
-                    : nil
-                
-                // TODO: simplify? use set?
-                if westGroup != nil {
-                    if northGroup != nil {
-                        if westGroup != northGroup {
-                            //merge groups
-                            
-                            var westGroupEquivalence: Set<Int>?
-                            var westGroupEquivalenceIndex: Int?
-                            var northGroupEquivalence: Set<Int>?
-                            var northGroupEquivalenceIndex: Int?
-                            for (index, equivalentGroup) in equivalentGroups.enumerated() {
-                                if equivalentGroup.contains(westGroup!) {
-                                    westGroupEquivalence = equivalentGroup
-                                    westGroupEquivalenceIndex = index
-                                }
-                                if equivalentGroup.contains(northGroup!) {
-                                    northGroupEquivalence = equivalentGroup
-                                    northGroupEquivalenceIndex = index
-                                }
-                            }
-                            
-                            if westGroupEquivalence == nil && northGroupEquivalence == nil {
-                                equivalentGroups.append([westGroup!, northGroup!])
-                            } else if westGroupEquivalence != nil && northGroupEquivalence != nil {
-                                if westGroupEquivalence != northGroupEquivalence {
-                                    equivalentGroups[westGroupEquivalenceIndex!].formUnion(northGroupEquivalence!)
-                                    equivalentGroups.remove(at: northGroupEquivalenceIndex!)
-                                }
-                            } else if westGroupEquivalence != nil {
-                                equivalentGroups[westGroupEquivalenceIndex!].insert(northGroup!)
-                            } else if northGroupEquivalence != nil {
-                                equivalentGroups[northGroupEquivalenceIndex!].insert(westGroup!)
-                            } else {
-                                assert(false) // shouldn't get here
-                            }
-                        }
-                        groupSizes[northGroup!]! += 1
-                        groupIds[y][x] = northGroup!
-                    } else {
-                        groupSizes[westGroup!]! += 1
-                        groupIds[y][x] = westGroup!
-                    }
-                } else if northGroup != nil {
-                    groupSizes[northGroup!]! += 1
-                    groupIds[y][x] = northGroup!
-                } else {
-                    //NEW GROUP
-                    var newGroup: Int
-                    if occupied {
-                        newGroup = occupiedGroup
-                        occupiedGroup += 1
-                    } else {
-                        newGroup = emptyGroup
-                        emptyGroup -= 1
-                    }
-                    groupIds[y][x] = newGroup
-                    groupSizes[newGroup] = 1
-                    groupToPoint[newGroup] = (x, y)
-                }
-            }
-        }
-        
-        for equivalentGroup in equivalentGroups {
-            let first = equivalentGroup.first
-            for group in equivalentGroup {
-                if group != first! {
-                    groupSizes[first!]! += groupSizes[group]!
-                    groupSizes[group] = nil
-                }
-            }
-        }
-        
-        let groupsAndSizes = groupSizes.sorted { $0.1 > $1.1 }
+        let groupsAndSizes = connectedComponentsInfo.labelToSize.sorted { $0.1 > $1.1 }
         var leafFound = false; // assume the biggest blob is leaf, second is the scale
         var scaleGroup: Int?
         for groupAndSize in groupsAndSizes {
@@ -233,13 +140,8 @@ class ThresholdingViewController: UIViewController, UIScrollViewDelegate {
         
         if scaleGroup != nil {
             // TODO: we should be using scale class not group
-            //var scaleClass: Set<Int>
-            for equivalentGroup in equivalentGroups {
-                if equivalentGroup.contains(scaleGroup!) {
-                    //scaleClass = equivalentGroup
-                }
-            }
-            let (xStart, yStart) = groupToPoint[scaleGroup!]!
+            
+            let (xStart, yStart) = connectedComponentsInfo.labelToMemberPoint[scaleGroup!]!
             
             let a = getFarthestPointInComponent(inImage: image, fromPoint: CGPoint(x: xStart, y: yStart))
             let b = getFarthestPointInComponent(inImage: image, fromPoint: a)
