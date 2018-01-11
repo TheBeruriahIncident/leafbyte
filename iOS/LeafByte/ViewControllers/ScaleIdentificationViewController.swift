38118f506864429346d7f541d357ac675710b680
Adam Campbell <adamc@palantir.com>
2018-01-11 00:03:40 -0500

    Beginning of scale identification works

--- iOS/LeafByte/ViewControllers/ScaleIdentificationViewController.swift
+++ iOS/LeafByte/ViewControllers/ScaleIdentificationViewController.swift
@@ -19,6 +19,10 @@ class ScaleIdentificationViewController: UIViewController, UIScrollViewDelegate 
     // The current mode can be scrolling or identifying.
     var inScrollingMode = true
     
+    // This is the number of pixels across the scale mark in the image.
+    // It's calculated in this view (if possible) and passed forward.
+    var scaleMarkPixelLength: Int?
+    
     // MARK: - Outlets
     
     @IBOutlet weak var gestureRecognizingView: UIScrollView!
@@ -37,7 +41,9 @@ class ScaleIdentificationViewController: UIViewController, UIScrollViewDelegate 
         setScrollingMode(!inScrollingMode)
     }
     @IBAction func clearScale(_ sender: Any) {
-        
+        scaleMarkPixelLength = nil
+        scaleMarkingView.image = nil
+        resultsText.text = "No scale"
     }
     
     // MARK: - UIViewController overrides
@@ -51,6 +57,7 @@ class ScaleIdentificationViewController: UIViewController, UIScrollViewDelegate 
         baseImageView.image = image
 
         setScrollingMode(true)
+        findScaleMark()
     }
     
     // This is called before transitioning from this view to another view.
@@ -64,7 +71,7 @@ class ScaleIdentificationViewController: UIViewController, UIScrollViewDelegate 
             
             destination.sourceType = sourceType
             destination.image = image
-            //destination.scaleMarkPixelLength = scaleMarkPixelLength
+            destination.scaleMarkPixelLength = scaleMarkPixelLength
         }
     }
     
@@ -77,12 +84,14 @@ class ScaleIdentificationViewController: UIViewController, UIScrollViewDelegate 
     // MARK: - UIResponder overrides
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // No drawing in scrolling mode.
+        // Touches don't matter in scrolling mode.
         if inScrollingMode {
             return
         }
 
         let candidatePoint = (touches.first?.location(in: baseImageView))!
+        print(candidatePoint)
+        print(baseImageView.image?.size)
     }
     
     // MARK: - Helpers
@@ -93,9 +102,55 @@ class ScaleIdentificationViewController: UIViewController, UIScrollViewDelegate 
         gestureRecognizingView.isUserInteractionEnabled = inScrollingMode
 
         if inScrollingMode {
-            modeToggleButton.setTitle("Switch to drawing", for: .normal)
+            modeToggleButton.setTitle("Touch the scale", for: .normal)
         } else {
-            modeToggleButton.setTitle("Switch to scrolling", for: .normal)
+            modeToggleButton.setTitle("Back to scrolling", for: .normal)
         }
+    }
+    
+    func findScaleMark() {
+        let indexableImage = IndexableImage(uiToCgImage(baseImageView.image!))
+        let image = BooleanIndexableImage(width: indexableImage.width, height: indexableImage.height)
+        image.addImage(indexableImage, withPixelToBoolConversion: { $0.isNonWhite() })
+        
+        let connectedComponentsInfo = labelConnectedComponents(image: image)
+        
+        // We're going to find the second biggest occupied component; we assume the biggest is the leaf and the second biggest is the scale mark.
+        // As such, filter down to just occupied components.
+        let occupiedLabelsAndSizes: [Int: Int] = connectedComponentsInfo.labelToSize.filter { $0.0 > 0 }
+        
+        // If we have less than two, we don't have a scale mark.
+        if occupiedLabelsAndSizes.count < 2 {
+            resultsText.text = "Scale not found"
+            return
+        }
+        
+        // The scale mark is the second biggest label.
+        let scaleMarkLabel = occupiedLabelsAndSizes.sorted { $0.1 > $1.1 }[1].key
+        
+        // Get a point in the scale mark.
+        let (scaleMarkPointX, scaleMarkPointY) = connectedComponentsInfo.labelToMemberPoint[scaleMarkLabel]!
+        
+        // Find the farthest point in the scale mark away, then the farthest away from that.
+        // This represents the farthest apart two points in the scale mark (where farthest refers to the path through the scale mark).
+        // This definition of farthest will work for us for thin, straight scale marks, which is what we expect.
+        let farthestPoint1 = getFarthestPointInComponent(inImage: indexableImage, fromPoint: CGPoint(x: scaleMarkPointX, y: scaleMarkPointY))
+        let farthestPoint2 = getFarthestPointInComponent(inImage: indexableImage, fromPoint: farthestPoint1)
+        
+        let candidateScaleMarkPixelLength = Int(round(farthestPoint1.distance(to: farthestPoint2)))
+        // If the scale mark is less than 5 pixels long, it's probably just noise in the image.
+        if candidateScaleMarkPixelLength < 5 {
+            resultsText.text = "Scale not found"
+            return
+        }
+        
+        scaleMarkPixelLength = candidateScaleMarkPixelLength
+        resultsText.text = "Scale found"
+        
+        // Draw a line where we think the scale mark is.
+        let drawingManager = DrawingManager(withCanvasSize: scaleMarkingView.frame.size, withProjection: Projection(fromImageInView: baseImageView.image!, toView: baseImageView))
+        drawingManager.getContext().setStrokeColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        drawingManager.drawLine(from: farthestPoint1, to: farthestPoint2)
+        drawingManager.finish(imageView: scaleMarkingView)
     }
 }
--- iOS/LeafByte/ViewControllers/ThresholdingViewController.swift
+++ iOS/LeafByte/ViewControllers/ThresholdingViewController.swift
@@ -18,10 +18,6 @@ class ThresholdingViewController: UIViewController, UIScrollViewDelegate {
     
     let filter = ThresholdingFilter()
     
-    // This is the number of pixels across the scale mark in the image.
-    // It's calculated in this view (if possible) and passed forward.
-    var scaleMarkPixelLength: Int?
-    
     // MARK: - Outlets
     
     @IBOutlet weak var gestureRecognizingView: UIScrollView!
@@ -84,53 +80,5 @@ class ThresholdingViewController: UIViewController, UIScrollViewDelegate {
     func setThreshold(_ threshold: Float) {
         filter.threshold = threshold
         baseImageView.image = ciToUiImage(filter.outputImage)
-        findScaleMark()
-    }
-    
-    func findScaleMark() {
-        let indexableImage = IndexableImage(uiToCgImage(baseImageView.image!))
-        let image = BooleanIndexableImage(width: indexableImage.width, height: indexableImage.height)
-        image.addImage(indexableImage, withPixelToBoolConversion: { $0.isNonWhite() })
-        
-        let connectedComponentsInfo = labelConnectedComponents(image: image)
-        
-        // We're going to find the second biggest occupied component; we assume the biggest is the leaf and the second biggest is the scale mark.
-        // As such, filter down to just occupied components.
-        let occupiedLabelsAndSizes: [Int: Int] = connectedComponentsInfo.labelToSize.filter { $0.0 > 0 }
-        
-        // If we have less than two, we don't have a scale mark.
-        if occupiedLabelsAndSizes.count < 2 {
-            // Remove any previously drawn mark before returning.
-            scaleMarkingView.image = nil
-            return
-        }
-        
-        // The scale mark is the second biggest label.
-        let scaleMarkLabel = occupiedLabelsAndSizes.sorted { $0.1 > $1.1 }[1].key
-        
-        // Get a point in the scale mark.
-        let (scaleMarkPointX, scaleMarkPointY) = connectedComponentsInfo.labelToMemberPoint[scaleMarkLabel]!
-        
-        // Find the farthest point in the scale mark away, then the farthest away from that.
-        // This represents the farthest apart two points in the scale mark (where farthest refers to the path through the scale mark).
-        // This definition of farthest will work for us for thin, straight scale marks, which is what we expect.
-        let farthestPoint1 = getFarthestPointInComponent(inImage: indexableImage, fromPoint: CGPoint(x: scaleMarkPointX, y: scaleMarkPointY))
-        let farthestPoint2 = getFarthestPointInComponent(inImage: indexableImage, fromPoint: farthestPoint1)
-        
-        let candidateScaleMarkPixelLength = Int(round(farthestPoint1.distance(to: farthestPoint2)))
-        // If the scale mark is less than 5 pixels long, it's probably just noise in the image.
-        if candidateScaleMarkPixelLength < 5 {
-            // Remove any previously drawn mark before returning.
-            scaleMarkingView.image = nil
-            return
-        }
-        
-        scaleMarkPixelLength = candidateScaleMarkPixelLength
-        
-        // Draw a line where we think the scale mark is.
-        let drawingManager = DrawingManager(withCanvasSize: scaleMarkingView.frame.size, withProjection: Projection(fromImageInView: baseImageView.image!, toView: baseImageView))
-        drawingManager.getContext().setStrokeColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
-        drawingManager.drawLine(from: farthestPoint1, to: farthestPoint2)
-        drawingManager.finish(imageView: scaleMarkingView)
     }
 }
