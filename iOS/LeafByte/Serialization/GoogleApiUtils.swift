484991cfa43af1e76dd280919b8b31bd7724de8f
Adam Campbell <adamc@palantir.com>
2018-01-21 10:59:42 -0500

    Pull out common create code

--- iOS/LeafByte/Serialization/GoogleApiUtils.swift
+++ iOS/LeafByte/Serialization/GoogleApiUtils.swift
@@ -7,17 +7,11 @@
 //
 
 func createFolder(name: String, accessToken: String, actionWithFolderId: @escaping (String) -> Void) {
-    post(url: "https://www.googleapis.com/drive/v2/files",
-        accessToken: accessToken,
-        jsonBody: "{title: \"\(name)\", mimeType: \"application/vnd.google-apps.folder\"}",
-        actionWithResponse: { response in actionWithFolderId(response["id"] as! String) })
+    createFile(name: name, folderId: nil, type: "folder", accessToken: accessToken, actionWithId: actionWithFolderId)
 }
 
 func createSheet(name: String, folderId: String, accessToken: String, actionWithSpreadsheetId: @escaping (String) -> Void) {
-    post(url: "https://www.googleapis.com/drive/v2/files",
-         accessToken: accessToken,
-         jsonBody: "{title: \"\(name)\", parents: [{id: \"\(folderId)\"}], mimeType: \"application/vnd.google-apps.spreadsheet\"}",
-         actionWithResponse: { response in actionWithSpreadsheetId(response["id"] as! String) })
+    createFile(name: name, folderId: folderId, type: "spreadsheet", accessToken: accessToken, actionWithId: actionWithSpreadsheetId)
 }
 
 func appendToSheet(spreadsheetId: String, row: [String], accessToken: String) {
@@ -25,4 +19,15 @@ func appendToSheet(spreadsheetId: String, row: [String], accessToken: String) {
     post(url: "https://sheets.googleapis.com/v4/spreadsheets/\(spreadsheetId)/values/Sheet1:append?valueInputOption=RAW&insertDataOption=INSERT_ROWS",
         accessToken: accessToken,
         jsonBody: "{values: [[\(formattedRow)]]}")
+}
+
+private func createFile(name: String, folderId: String?, type: String, accessToken: String, actionWithId: @escaping (String) -> Void) {
+    let parentsParam = folderId != nil
+        ? " parents: [{id: \"\(folderId!)\"}],"
+        : ""
+    
+    post(url: "https://www.googleapis.com/drive/v2/files",
+         accessToken: accessToken,
+         jsonBody: "{title: \"\(name)\",\(parentsParam) mimeType: \"application/vnd.google-apps.\(type)\"}",
+        actionWithResponse: { response in actionWithId(response["id"] as! String) })
 }
