ca543b238a1d8b68ecb459e61b9a21788d3d1fea
Adam Campbell <adamc@palantir.com>
2018-01-22 23:29:45 -0500

    Revert "Remove cropping"
    This reverts commit 38830a7d3da5f835bb7dce93c6349472a4a8c36e.

--- iOS/LeafByte/Utils/UIUtils.swift
+++ iOS/LeafByte/Utils/UIUtils.swift
@@ -11,7 +11,8 @@ import UIKit
 
 func finishWithImagePicker(self viewController: UIViewController, info: [String : Any], selectImage: (CGImage) -> Void) {
     // There may (in theory) contain multiple versions of the image in info; we're not allowing editing, so just take the original.
-    guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+    // TODO: switch back to original
+    guard let selectedImage = info[UIImagePickerControllerEditedImage] as? UIImage else {
         fatalError("Expected to find an image under UIImagePickerControllerEditedImage in \(info)")
     }
     
@@ -66,5 +67,5 @@ func setupImagePicker(imagePicker: UIImagePickerController, self viewController:
     // Allowing editing to get easy cropping is tempting, but cropping in the image picker is broken in various ways on different devices.
     // For example, on most devices, the crop window will be applied ~10% above where the user chooses, and on some devices, the crop window won't be movable at all.
     // TODO: stuff is breaking when I switch back to false, figure
-    imagePicker.allowsEditing = false
+    imagePicker.allowsEditing = true
 }
