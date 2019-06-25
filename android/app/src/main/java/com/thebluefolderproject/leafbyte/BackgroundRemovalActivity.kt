aba48b4b5818909e345d2f6884bdfc8147ea07b7
Adam Campbell <adam.t.campbell@gmail.com>
2019-06-24 21:50:13 -0400

    Add BackgroundRemovalActivity

--- android/app/src/main/java/com/thebluefolderproject/leafbyte/BackgroundRemovalActivity.kt
+++ android/app/src/main/java/com/thebluefolderproject/leafbyte/BackgroundRemovalActivity.kt
@@ -3,10 +3,10 @@ package com.thebluefolderproject.leafbyte
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainMenuActivity : AppCompatActivity() {
+class BackgroundRemovalActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_background_removal)
     }
 }
