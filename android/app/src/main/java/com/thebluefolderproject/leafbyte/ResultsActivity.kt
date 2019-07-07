75093e1a5ada17ed890859e4e36284ef4e8023bf
Adam Campbell <adam.t.campbell@gmail.com>
2019-07-07 14:23:31 -0400

    Add ResultsActivity

---
+++ android/app/src/main/java/com/thebluefolderproject/leafbyte/ResultsActivity.kt
@@ -0,0 +1,12 @@
+package com.thebluefolderproject.leafbyte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_results)
+    }
+}
