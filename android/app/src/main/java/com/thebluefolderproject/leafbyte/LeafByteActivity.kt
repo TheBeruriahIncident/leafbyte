9c7c67b43cf86c08033ca7216e947b6fad44f0c8
Adam Campbell <adam.t.campbell@gmail.com>
2019-07-14 20:38:22 -0400

    Add nav graph

---
+++ android/app/src/main/java/com/thebluefolderproject/leafbyte/LeafByteActivity.kt
@@ -0,0 +1,12 @@
+package com.thebluefolderproject.leafbyte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeafByteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leaf_byte)
+    }
+}
