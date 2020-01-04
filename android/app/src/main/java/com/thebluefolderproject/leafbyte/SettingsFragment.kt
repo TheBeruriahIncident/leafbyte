698601b3e1c12202d8db1f62796ec43277e4e9cd
Adam Campbell <adam.t.campbell@gmail.com>
2020-01-03 19:28:10 -0500

    clunky version of "use previous dataset"

--- android/app/src/main/java/com/thebluefolderproject/leafbyte/SettingsFragment.kt
+++ android/app/src/main/java/com/thebluefolderproject/leafbyte/SettingsFragment.kt
@@ -9,7 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProviders
+import androidx.preference.EditTextPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
@@ -32,5 +34,25 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        val button: Preference = preferenceManager.findPreference("use_previous_dataset")!!
+        button.setOnPreferenceClickListener {
+            debug("clicked")
+
+            val builder = AlertDialog.Builder(context!!)
+            //builder.setTitle("Choose an animal")
+
+            val animals = arrayOf("Hello", "Goodbye")
+            builder.setItems(animals) { dialog, which ->
+                //commit vs apply
+                preferenceManager.sharedPreferences.edit().putString("dataset_name_preference", animals.get(which)).apply()
+                //setPreferencesFromResource(R.xml.preferences, rootKey)
+            }
+
+            val dialog = builder.create()
+            dialog.show()
+
+            true
+        }
     }
 }
