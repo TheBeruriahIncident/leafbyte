e1e9abe49ba101e7cd409351c28ddb858c1cde45
Adam Campbell <adam.t.campbell@gmail.com>
2020-01-04 17:46:31 -0500

    Scale length summary with units

--- android/app/src/main/java/com/thebluefolderproject/leafbyte/SettingsFragment.kt
+++ android/app/src/main/java/com/thebluefolderproject/leafbyte/SettingsFragment.kt
@@ -1,29 +1,14 @@
 package com.thebluefolderproject.leafbyte
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
-import androidx.core.widget.addTextChangedListener
-import androidx.lifecycle.ViewModelProviders
 import androidx.preference.EditTextPreference
+import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import java.lang.Exception
-
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -43,18 +28,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
         val button: Preference = preferenceManager.findPreference("use_previous_dataset")!!
         button.setOnPreferenceClickListener {
             val builder = AlertDialog.Builder(context!!)
-            //builder.setTitle("Choose an animal")
-
-            val animals = arrayOf("Hello", "Goodbye")
-            builder.setItems(animals) { dialog, which ->
-                //commit vs apply
-                //preferenceManager.sharedPreferences.edit().putString("dataset_name_preference", animals.get(which)).apply()
-                //setPreferencesFromResource(R.xml.preferences, rootKey)
-
-                // HACKHACK
-                //onCreatePreferences(savedInstanceState, rootKey)
-
-                datasetName.text = animals[which]
+            val options = arrayOf("Hello", "Goodbye")
+            builder.setItems(options) { dialog, which ->
+                datasetName.text = options[which]
             }
 
             val dialog = builder.create()
@@ -63,16 +39,17 @@ class SettingsFragment : PreferenceFragmentCompat() {
             true
         }
 
+        val scaleLengthUnitPreference: ListPreference = preferenceManager.findPreference("units_preference")!!
 
         val scaleLengthPreference: EditTextPreference = preferenceManager.findPreference("scale_length_preference")!!
         // derived from https://stackoverflow.com/a/59297100/1092672
-        scaleLengthPreference.setOnBindEditTextListener(EditTextPreference.OnBindEditTextListener { editText ->
+        scaleLengthPreference.setOnBindEditTextListener { editText ->
             try {
                 editText.text.toString().toDouble()
             } catch (e: NumberFormatException) {
                 editText.error = e.localizedMessage;
-//                        editText.rootView.findViewById(android.R.id.button1)
-//                            .setEnabled(validationError == null);
+    //                        editText.rootView.findViewById(android.R.id.button1)
+    //                            .setEnabled(validationError == null);
             }
 
             editText.addTextChangedListener(object : TextWatcher {
@@ -83,8 +60,8 @@ class SettingsFragment : PreferenceFragmentCompat() {
                         editText.error = e.localizedMessage;
 
 
-//                        editText.rootView.findViewById(android.R.id.button1)
-//                            .setEnabled(validationError == null);
+    //                        editText.rootView.findViewById(android.R.id.button1)
+    //                            .setEnabled(validationError == null);
                     }
                 }
 
@@ -94,6 +71,20 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 }
             })
-        })
+        }
+        scaleLengthPreference.summary = scaleLengthPreference.text + " " + scaleLengthUnitPreference.value
+        scaleLengthPreference.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference: Preference?, newValue: Any? ->
+            val newValueString: String = newValue as String;
+            // TODO: dont dupe this
+            scaleLengthPreference.summary = newValueString + " " + scaleLengthUnitPreference.value
+            true
+        }
+
+        scaleLengthUnitPreference.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference: Preference?, newValue: Any? ->
+            val newValueString: String = newValue as String;
+            // TODO: dont dupe this
+            scaleLengthPreference.summary = scaleLengthPreference.text + " " + newValueString
+            true
+        }
     }
 }
