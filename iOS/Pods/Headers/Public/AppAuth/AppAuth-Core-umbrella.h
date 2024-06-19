7dbd9093aa823501fa235d2ebb756f490de0b761
Abigail Getman-Pickering <abigail.getman.pickering@gmail.com>
2024-06-18 20:32:29 -0400

    Make dependencies in tests compile again

---
+++ iOS/Pods/Headers/Public/AppAuth/AppAuth-Core-umbrella.h
@@ -0,0 +1,1 @@
+../../../Target Support Files/AppAuth-Core/AppAuth-Core-umbrella.h
---
+++ iOS/Pods/Target Support Files/AppAuth-Core/AppAuth-Core-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AppAuth_Core : NSObject
+@end
+@implementation PodsDummy_AppAuth_Core
+@end
