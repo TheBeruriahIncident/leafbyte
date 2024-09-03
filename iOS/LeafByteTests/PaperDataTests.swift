0bb1089c0f4917c9c8e74b07acfdb326aa9ed959
Abigail Getman-Pickering <abigail.getman.pickering@gmail.com>
2024-09-02 23:46:53 -0400

    lint

--- iOS/LeafByteTests/PaperDataTests.swift
+++ iOS/LeafByteTests/PaperDataTests.swift
@@ -15,10 +15,11 @@ final class PaperDataTests: XCTestCase {
     private let scaleMarkLengthCm: Double = 17
 
     func testPaperData() {
+        // swiftlint:disable:next todo
         // TODO: several files are commented out, because scale identification fails. we should fix that, or delete those lines and the associated images
 
-        //testIndividualFile(filename: "AI2", expectedTotalArea: 22.565, expectedConsumedArea: 2.279)
-        //testIndividualFile(filename: "CA1", expectedTotalArea: 9.791, expectedConsumedArea: 0.000)
+        // testIndividualFile(filename: "AI2", expectedTotalArea: 22.565, expectedConsumedArea: 2.279)
+        // testIndividualFile(filename: "CA1", expectedTotalArea: 9.791, expectedConsumedArea: 0.000)
         testIndividualFile(filename: "CO1", expectedTotalArea: 23.951, expectedConsumedArea: 0.992)
         testIndividualFile(filename: "CO2", expectedTotalArea: 25.231, expectedConsumedArea: 1.154)
         testIndividualFile(filename: "CO5", expectedTotalArea: 27.016, expectedConsumedArea: 1.176)
@@ -27,25 +28,25 @@ final class PaperDataTests: XCTestCase {
         testIndividualFile(filename: "GB4", expectedTotalArea: 14.953, expectedConsumedArea: 2.901)
         testIndividualFile(filename: "GB5", expectedTotalArea: 17.474, expectedConsumedArea: 3.126)
         testIndividualFile(filename: "GB6", expectedTotalArea: 22.048, expectedConsumedArea: 6.008)
-        //testIndividualFile(filename: "IE1", expectedTotalArea: 7.570, expectedConsumedArea: 0.000)
+        // testIndividualFile(filename: "IE1", expectedTotalArea: 7.570, expectedConsumedArea: 0.000)
         testIndividualFile(filename: "MP5", expectedTotalArea: 45.924, expectedConsumedArea: 1.889)
         testIndividualFile(filename: "OR2", expectedTotalArea: 52.046, expectedConsumedArea: 1.318)
-        //testIndividualFile(filename: "PB2", expectedTotalArea: 53.341, expectedConsumedArea: 2.096)
+        // testIndividualFile(filename: "PB2", expectedTotalArea: 53.341, expectedConsumedArea: 2.096)
         testIndividualFile(filename: "SH2", expectedTotalArea: 9.936, expectedConsumedArea: 0.424)
         testIndividualFile(filename: "SH4", expectedTotalArea: 5.733, expectedConsumedArea: 0.002)
         testIndividualFile(filename: "SH5", expectedTotalArea: 10.136, expectedConsumedArea: 0.354)
-        //testIndividualFile(filename: "SP5", expectedTotalArea: 11.411, expectedConsumedArea: 0.967)
-        //testIndividualFile(filename: "SS1", expectedTotalArea: 5.898, expectedConsumedArea: 0.538)
-        //testIndividualFile(filename: "SS2", expectedTotalArea: 4.920, expectedConsumedArea: 0.208)
+        // testIndividualFile(filename: "SP5", expectedTotalArea: 11.411, expectedConsumedArea: 0.967)
+        // testIndividualFile(filename: "SS1", expectedTotalArea: 5.898, expectedConsumedArea: 0.538)
+        // testIndividualFile(filename: "SS2", expectedTotalArea: 4.920, expectedConsumedArea: 0.208)
         testIndividualFile(filename: "SS3", expectedTotalArea: 4.853, expectedConsumedArea: 0.000)
         testIndividualFile(filename: "SS4", expectedTotalArea: 6.502, expectedConsumedArea: 0.815)
         testIndividualFile(filename: "TH3", expectedTotalArea: 25.632, expectedConsumedArea: 2.422)
-        //testIndividualFile(filename: "TH4", expectedTotalArea: 39.763, expectedConsumedArea: 0.001)
+        // testIndividualFile(filename: "TH4", expectedTotalArea: 39.763, expectedConsumedArea: 0.001)
         testIndividualFile(filename: "TH5", expectedTotalArea: 30.008, expectedConsumedArea: 1.560)
         testIndividualFile(filename: "TM1", expectedTotalArea: 41.100, expectedConsumedArea: 2.351)
         testIndividualFile(filename: "TM2", expectedTotalArea: 60.034, expectedConsumedArea: 0.002)
         testIndividualFile(filename: "TM4", expectedTotalArea: 53.855, expectedConsumedArea: 2.678)
-        //testIndividualFile(filename: "TM5", expectedTotalArea: 42.391, expectedConsumedArea: 3.125)
+        // testIndividualFile(filename: "TM5", expectedTotalArea: 42.391, expectedConsumedArea: 3.125)
     }
 
     private func testIndividualFile(filename: String, expectedTotalArea: Double, expectedConsumedArea: Double) {
