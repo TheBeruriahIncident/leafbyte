3b4a932345d4993f2574f4698d4f72e709977d69
Abigail Getman-Pickering <abigail.getman.pickering@gmail.com>
2025-06-26 09:56:48 -0400

    try toolchain resolver

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,2 +1,8 @@
 include(":app")
 include(":openCVLibrary343")
+
+plugins {
+    // we need this so the build on Renovate Bot's machines will download Java
+    // version catalog doesn't seem to work for convention plugins
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
