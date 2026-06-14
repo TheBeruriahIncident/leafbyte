0c35b6c1f82487631f03c9444890d597c1e5880e
Abigail Getman-Pickering <abigail.getman.pickering@gmail.com>
2026-06-13 21:47:41 -0400

    Make Kotlin Metadata use platform version

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
         classpath(libs.android.gradle)
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
         // delete this once https://github.com/autonomousapps/dependency-analysis-gradle-plugin/issues/1661 is resolved
-        classpath("org.jetbrains.kotlin:kotlin-metadata-jvm:2.3.20")
+        classpath(libs.kotlin.metadata)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
